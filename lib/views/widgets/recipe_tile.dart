dfa3515f1caacb6b6ba541fa27b8a1f14ee54a6c
mrezkys <mrezkysulihin@gmail.com>
2021-09-06 12:23:55 +0800

    feat: add push navigate to RecipeDetailPage

--- lib/views/widgets/recipe_tile.dart
+++ lib/views/widgets/recipe_tile.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:hungry/models/core/recipe.dart';
+import 'package:hungry/views/screens/recipe_detail_page.dart';
 import 'package:hungry/views/utils/AppColor.dart';
 
 class RecipeTile extends StatelessWidget {
@@ -9,80 +10,85 @@ class RecipeTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 90,
-      padding: EdgeInsets.all(10),
-      decoration: BoxDecoration(
-        color: AppColor.whiteSoft,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Row(
-        children: [
-          // Recipe Photo
-          Container(
-            width: 70,
-            height: 70,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(5),
-              color: Colors.blueGrey,
-              image: DecorationImage(image: AssetImage(data.photo)),
-            ),
-          ),
-          // Recipe Info
-          Expanded(
-            child: Container(
-              margin: EdgeInsets.only(left: 10),
-              padding: EdgeInsets.symmetric(horizontal: 10),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  // Recipe title
-                  Container(
-                    margin: EdgeInsets.only(bottom: 12),
-                    child: Text(
-                      data.title,
-                      style: TextStyle(fontWeight: FontWeight.w600, fontFamily: 'inter'),
-                    ),
-                  ),
-                  // Recipe Calories and Time
-                  Row(
-                    children: [
-                      SvgPicture.asset(
-                        'assets/icons/fire-filled.svg',
-                        color: Colors.black,
-                        width: 12,
-                        height: 12,
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(left: 5),
-                        child: Text(
-                          data.calories,
-                          style: TextStyle(fontSize: 12),
-                        ),
-                      ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Icon(
-                        Icons.alarm,
-                        size: 14,
-                        color: Colors.black,
-                      ),
-                      Container(
-                        margin: EdgeInsets.only(left: 5),
-                        child: Text(
-                          data.time,
-                          style: TextStyle(fontSize: 12),
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(builder: (context) => RecipeDetailPage(data: data)));
+      },
+      child: Container(
+        height: 90,
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: AppColor.whiteSoft,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          children: [
+            // Recipe Photo
+            Container(
+              width: 70,
+              height: 70,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(5),
+                color: Colors.blueGrey,
+                image: DecorationImage(image: AssetImage(data.photo)),
               ),
             ),
-          ),
-        ],
+            // Recipe Info
+            Expanded(
+              child: Container(
+                margin: EdgeInsets.only(left: 10),
+                padding: EdgeInsets.symmetric(horizontal: 10),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // Recipe title
+                    Container(
+                      margin: EdgeInsets.only(bottom: 12),
+                      child: Text(
+                        data.title,
+                        style: TextStyle(fontWeight: FontWeight.w600, fontFamily: 'inter'),
+                      ),
+                    ),
+                    // Recipe Calories and Time
+                    Row(
+                      children: [
+                        SvgPicture.asset(
+                          'assets/icons/fire-filled.svg',
+                          color: Colors.black,
+                          width: 12,
+                          height: 12,
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(left: 5),
+                          child: Text(
+                            data.calories,
+                            style: TextStyle(fontSize: 12),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10,
+                        ),
+                        Icon(
+                          Icons.alarm,
+                          size: 14,
+                          color: Colors.black,
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(left: 5),
+                          child: Text(
+                            data.time,
+                            style: TextStyle(fontSize: 12),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
