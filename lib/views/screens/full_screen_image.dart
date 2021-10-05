c0f027b5b2a577dca5088b0f2db2f4068020e816
mrezkys <mrezkysulihin@gmail.com>
2021-10-05 21:20:27 +0800

    feat: fullscreenimage

---
+++ lib/views/screens/full_screen_image.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class FullScreenImage extends StatelessWidget {
+  final Widget image;
+  FullScreenImage({@required this.image});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        brightness: Brightness.dark,
+        backgroundColor: Colors.black,
+        elevation: 0,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios, color: Colors.white),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        alignment: Alignment.center,
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          child: image,
+        ),
+      ),
+    );
+  }
+}
