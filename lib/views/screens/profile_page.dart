6afbda139557cdbd62e1adc2e7a9c77541283d54
mrezkys <mrezkysulihin@gmail.com>
2021-10-05 21:17:49 +0800

    feat: profile page

---
+++ lib/views/screens/profile_page.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:hungry/views/utils/AppColor.dart';
+import 'package:hungry/views/widgets/user_info_tile.dart';
+
+class ProfilePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        brightness: Brightness.dark,
+        backgroundColor: AppColor.primary,
+        elevation: 0,
+        centerTitle: true,
+        title: Text('My Profile', style: TextStyle(fontFamily: 'inter', fontWeight: FontWeight.w400, fontSize: 16)),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios, color: Colors.white),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              'Edit',
+              style: TextStyle(color: Colors.white, fontSize: 14, fontWeight: FontWeight.w600),
+            ),
+            style: TextButton.styleFrom(primary: Colors.white, shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100))),
+          ),
+        ],
+      ),
+      body: ListView(
+        shrinkWrap: true,
+        physics: BouncingScrollPhysics(),
+        children: [
+          Container(
+            color: AppColor.primary,
+            padding: EdgeInsets.symmetric(vertical: 24),
+            child: GestureDetector(
+              onTap: () {
+                print('Code to open file manager');
+              },
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 130,
+                    height: 130,
+                    margin: EdgeInsets.only(bottom: 15),
+                    decoration: BoxDecoration(
+                      color: Colors.grey,
+                      borderRadius: BorderRadius.circular(100),
+                      // Profile Picture
+                      image: DecorationImage(image: AssetImage('assets/images/profile.jpg'), fit: BoxFit.cover),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text('Change Profile Picture', style: TextStyle(fontFamily: 'inter', fontWeight: FontWeight.w600, color: Colors.white)),
+                      SizedBox(width: 8),
+                      SvgPicture.asset('assets/icons/camera.svg', color: Colors.white),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 24),
+            width: MediaQuery.of(context).size.width,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                UserInfoTile(
+                  margin: EdgeInsets.only(bottom: 16),
+                  label: 'Email',
+                  value: 'reinazahradummy@gmail.com',
+                ),
+                UserInfoTile(
+                  margin: EdgeInsets.only(bottom: 16),
+                  label: 'Full Name',
+                  value: 'Reina Zahra Azizah',
+                ),
+                UserInfoTile(
+                  margin: EdgeInsets.only(bottom: 16),
+                  label: 'Subscription Type',
+                  value: 'Premium Subscription',
+                  valueBackground: AppColor.secondary,
+                ),
+                UserInfoTile(
+                  margin: EdgeInsets.only(bottom: 16),
+                  label: 'Subscription Time',
+                  value: 'Until 22 Oct 2021',
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
